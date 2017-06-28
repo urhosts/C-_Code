562a4b04f21b9c7e5a9245d55c82ba0b7e3835d4
urhosts <hosts@gsxio.com>
2017-06-28 12:08:43 +0800

    StringClass

---
+++ StringClass/main.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: hosts
+	> Mail: hosts@gsxio.com
+	> Created Time: Wed 28 Jun 2017 11:12:27 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include "String.h"
+using namespace std;
+
+int main()
+{
+    String a("abcdef");
+    return 0;
+}
