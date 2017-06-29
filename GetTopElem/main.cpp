d2c8777cc1f30bd216030488f5a37e750ec26969
urhosts <hosts@gsxio.com>
2017-06-29 23:57:47 +0800

    GetTop

---
+++ GetTopElem/main.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: hosts
+	> Mail: hosts@gsxio.com
+	> Created Time: Thu 29 Jun 2017 11:43:37 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include "GetTop.h"
+using namespace std;
+
+int main()
+{
+    TestFruitMap();
+    return 0;
+}
